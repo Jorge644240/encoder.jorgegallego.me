d054a2d99e3e587a12eeab116fafd37b9025cfce
Jorge Gallego <gallegojorge908@gmail.com>
2021-09-16 15:41:59 -0500

    Added feature to shorten URL

--- index.js
+++ index.js
@@ -1,13 +1,19 @@
 document.querySelector("#encode").addEventListener("click", () => {
     document.querySelector("#result").innerHTML = encodeURIComponent(document.querySelector("textarea").value);
-    document.querySelector("#result").target = "_blank";
-    document.querySelector("#result").href = document.querySelector("textarea").value;
+    if (document.querySelector("textarea").value.includes("http")) {
+        document.querySelector("#result").href = document.querySelector("textarea").value;
+        document.querySelector("#result").target = "_blank";
+    }
+    document.querySelector("#shorten").style.display = "block";
 });
 
 document.querySelector("#decode").addEventListener("click", () => {
     document.querySelector("#result").innerHTML = decodeURIComponent(document.querySelector("textarea").value);
-    document.querySelector("#result").target = "_blank";
-    document.querySelector("#result").href = decodeURIComponent(document.querySelector("textarea").value);
+    if (document.querySelector("textarea").value.includes("http")) {
+        document.querySelector("#result").href = decodeURIComponent(document.querySelector("textarea").value);
+        document.querySelector("#result").target = "_blank";
+    }
+    document.querySelector("#shorten").style.display = "block";
 });
 
 window.onload = window.onresize = () => {
@@ -19,3 +25,7 @@ window.onload = window.onresize = () => {
         document.querySelector("textarea").rows = 10;
     }
 }
+
+document.querySelector("#shorten").addEventListener("click", () => {
+    window.location.replace(`https://tinyurl.jorgegallego.me/shorten?url=${encodeURIComponent(document.querySelector("textarea").value)}`);
+});
